a9f0abbed7fe4ff3f28e25b0bb1880dae9626637
paullee714 <ba93love@gmail.com>
2020-09-06 17:19:13 +0900

    bootstrap 세팅

--- web/front/.eslintrc.js
+++ web/front/.eslintrc.js
@@ -1,40 +1,40 @@
 module.exports = {
     root: true,
     env: {
-        node: true
+        node: true,
     },
-    extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+    extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
     parserOptions: {
-        parser: "babel-eslint"
+        parser: 'babel-eslint',
     },
     rules: {
         // "no-console": "off",
-        "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-        "no-useless-escape": 0,
-        "prettier/prettier": [
-            "error",
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-useless-escape': 0,
+        'prettier/prettier': [
+            'error',
             {
                 singleQuote: true,
                 semi: true,
                 useTabs: false,
                 tabWidth: 4,
-                trailingComma: "all",
+                trailingComma: 'all',
                 printWidth: 80,
                 bracketSpacing: true,
-                arrowParens: "avoid"
-            }
-        ]
+                arrowParens: 'avoid',
+            },
+        ],
     },
     overrides: [
         {
             files: [
-                "**/__tests__/*.{j,t}s?(x)",
-                "**/tests/unit/**/*.spec.{j,t}s?(x)"
+                '**/__tests__/*.{j,t}s?(x)',
+                '**/tests/unit/**/*.spec.{j,t}s?(x)',
             ],
             env: {
-                jest: true
-            }
-        }
-    ]
+                jest: true,
+            },
+        },
+    ],
 };
--- web/front/src/App.vue
+++ web/front/src/App.vue
@@ -1,32 +1,14 @@
 <template>
-    <div id="app">
-        <div id="nav">
-            <router-link to="/">Home</router-link> |
-            <router-link to="/about">About</router-link>
+    <div>
+        <div class="row">
+            <div class="col-md-4" style="background-color: skyblue">
+                안녕하십니까!
+            </div>
+            <div class="col-md-8" style="background-color: yellowgreen">
+                반갑습니다!
+            </div>
         </div>
-        <router-view />
     </div>
 </template>
 
-<style>
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-}
-
-#nav {
-    padding: 30px;
-}
-
-#nav a {
-    font-weight: bold;
-    color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-    color: #42b983;
-}
-</style>
+<style></style>
