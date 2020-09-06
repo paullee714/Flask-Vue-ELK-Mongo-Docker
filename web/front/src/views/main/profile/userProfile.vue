c8141e55f0c23503efaa31ad1f061689e2899897
paullee714 <ba93love@gmail.com>
2020-09-06 19:50:11 +0900

    기본 페이지 작성

---
+++ web/front/src/views/main/profile/userProfile.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div>
+            <div class="card">
+                <img src="#" class="card-img-top" />
+                <div class="card-body">
+                    <h5 class="card-title">사용자이름</h5>
+                    <p class="card-text">
+                        프로필 정보
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'userProfile',
+};
+</script>
+
+<style scoped></style>
---
+++ web/front/src/views/navMenu/topnav.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="#">TweetSecret</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </nav>
+        <br />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'topnav',
+};
+</script>
+
+<style scoped></style>
