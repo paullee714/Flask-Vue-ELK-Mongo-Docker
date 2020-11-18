f9709156319252c3ab4f45cb979f3cecf1f64f08
paullee714 <ba93love@gmail.com>
2020-11-18 20:10:19 +0900

    몽고디비 도커 추가 설정하기

--- web/back/route/test_route.py
+++ web/back/route/test_route.py
@@ -2,10 +2,15 @@ from flask import Blueprint,jsonify
 
 # logging lib
 from util.my_log import back_logger_info
+from back_lib import connect_db
 
 my_test = Blueprint('test',__name__)
 
 @my_test.route('/',methods=['GET'])
 def test_router():
     back_logger_info('hello world api!')
+
+    client = connect_db.connect_mongo()
+    back_logger_info(client)
+
     return jsonify("hello world!")
